315370ce12ddf692e3e6604eda75c2afd7404afa
Albert Albala <albert@mediatribe.net>
2021-11-16 19:48:19 -0500

    2021-11-16-19:48:18

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,3 +8,7 @@ echo 'docker -v >= 19.03'
 docker -v
 echo 'linux kernel version, uname -r >= 4.8'
 uname -r
+echo '/proc/sys/fs/binfmt_misc/ must exist'
+ls -lah /proc/sys/fs/binfmt_misc/
+echo 'qemu-user-static'
+sudo apt-get install -y qemu-user-static

e03ef37a1971ef7338b8e39a849ac890a44b2546
Albert Albala <albert@mediatribe.net>
2021-11-16 22:33:12 -0500

    Do not output error we want to avoid, makes it look like the error is occurring

--- scripts/install-qemu-user-static.sh
+++ scripts/install-qemu-user-static.sh
@@ -5,8 +5,9 @@
 set -e
 
 echo "Must go through hoops because of https://cloudsupport.digitalocean.com/s/case/5004P00001hK6QuQAK/after-creating-droplet-could-not-get-lock-varlibdpkglockfrontend:"
-echo "E: Could not get lock /var/lib/dpkg/lock-frontend - open (11: Resource temporarily unavailable)"
-echo "E: Unable to acquire the dpkg frontend lock (/var/lib/dpkg/lock-frontend), is another process using it?"
+# Without this we will get
+# E: Could not get lock /var/lib/dpkg/lock-frontend - open (11: Resource temporarily unavailable)
+# E: Unable to acquire the dpkg frontend lock (/var/lib/dpkg/lock-frontend), is another process using it?
 sudo rm -f /var/lib/apt/lists/lock
 sudo rm -f /var/lib/apt/lists/lock-frontend
 sudo rm -f /var/lib/dpkg/lock-frontend

600336716203c529226fe14707b1e074527c8696
Albert Albala <albert@mediatribe.net>
2021-12-09 13:20:38 -0500

    Please squash this commit in the pull request

--- scripts/install-qemu-user-static.sh
+++ scripts/install-qemu-user-static.sh
@@ -4,12 +4,12 @@
 #
 set -e
 
-echo "Must go through hoops because of https://cloudsupport.digitalocean.com/s/case/5004P00001hK6QuQAK/after-creating-droplet-could-not-get-lock-varlibdpkglockfrontend:"
-# Without this we will get
-# E: Could not get lock /var/lib/dpkg/lock-frontend - open (11: Resource temporarily unavailable)
-# E: Unable to acquire the dpkg frontend lock (/var/lib/dpkg/lock-frontend), is another process using it?
-sudo rm -f /var/lib/apt/lists/lock
-sudo rm -f /var/lib/apt/lists/lock-frontend
-sudo rm -f /var/lib/dpkg/lock-frontend
-sudo rm -f /var/lib/dpkg/lock
-sudo sudo apt-get install -y qemu-user-static
+# We want the latest version, not the one on the package manager, to avoid
+# issues such as https://askubuntu.com/questions/1339558.
+# See https://www.qemu.org/download/.
+git clone https://gitlab.com/qemu-project/qemu.git
+cd qemu
+git submodule init
+git submodule update --recursive
+./configure
+make

ed6b7058e906b0bf369eebdce55e70c8d681edec
Albert Albala <albert@mediatribe.net>
2021-12-09 13:22:00 -0500

    Please squash this commit in the pull request

--- scripts/install-qemu-user-static.sh
+++ scripts/install-qemu-user-static.sh
@@ -7,7 +7,7 @@ set -e
 # We want the latest version, not the one on the package manager, to avoid
 # issues such as https://askubuntu.com/questions/1339558.
 # See https://www.qemu.org/download/.
-git clone https://gitlab.com/qemu-project/qemu.git
+git clone --depth 1 https://gitlab.com/qemu-project/qemu.git
 cd qemu
 git submodule init
 git submodule update --recursive

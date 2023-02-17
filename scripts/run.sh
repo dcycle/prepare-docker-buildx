2492b1b8e268bc103879e7f5d3e5c0ad39e2b08b
Albert Albala <albert@mediatribe.net>
2023-02-17 16:53:40 -0500

    Please squash this commit in the pull request

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,10 @@
 #
 # Make sure we are ready to run docker buildx
 #
-set -e
+set -ex
 
+echo "Starting in February 2023, we get ERROR: docker experimental flag not enabled: Set with 'export DOCKER_CLI_EXPERIMENTAL=enabled' even though that has been done in the calling script. So we're doing it again here."
+export DOCKER_CLI_EXPERIMENTAL=enabled
 echo 'See https://medium.com/@artur.klauser/building-multi-architecture-docker-images-with-buildx-27d80f7e2408'
 echo 'docker -v >= 19.03'
 docker -v
@@ -13,16 +15,11 @@ echo '/proc/sys/fs/binfmt_misc/ must exist'
 ls -lah /proc/sys/fs/binfmt_misc/
 echo 'Install qemu'
 source ./scripts/lib/install-qemu.source.sh
-echo 'ls -l /usr/bin/qemu-aarch64-static'
 ls -l /usr/bin/qemu-aarch64-static
-echo 'qemu-aarch64-static --version'
 qemu-aarch64-static --version
 echo 'update-binfmts --version >= 2.1.7'
 update-binfmts --version
-echo 'scripts/reregister-qemu-binfmt.sh'
 ./scripts/reregister-qemu-binfmt.sh
-echo 'check-qemu-binfmt.sh'
 ./scripts/check-qemu-binfmt.sh
 echo 'Qemu version, should be 6 or more to avoid issues such as https://askubuntu.com/questions/1339558'
-echo 'docker buildx'
 docker buildx
